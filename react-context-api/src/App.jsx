88f1d9e83584a5d5ed7fabd63bc7b4b83a051017
tizianotambone <tambonetiziano@gmail.com>
2025-05-19 21:03:25 +0200

    [UPD] creazione PostPage e routes

--- react-context-api/src/App.jsx
+++ react-context-api/src/App.jsx
@@ -1,34 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import PostPage from './components/PostPage';
+import postContext from './context/postContext';
+
+const listPosts = [
+    { id: 1, title: 'React Basis', content: 'React apps are made out of components. A component is a piece of the UI (user interface) that has its own logic and appearance. A component can be as small as a button, or as large as an entire page.', category: 'FrontEnd' },
+    { id: 2, title: 'What is Express.js?', content: 'Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.', category: 'Backend' },
+    { id: 3, title: 'Run JavaScript Everywhere', content: 'Node.js® is a free, open-source, cross-platform JavaScript runtime environment that lets developers create servers, web apps, command line tools and scripts.', category: 'Backend' },
+    { id: 4, title: 'Build fast, responsive sites with Bootstrap', content: 'Powerful, extensible, and feature-packed frontend toolkit. Build and customize with Sass, utilize prebuilt grid system and components, and bring projects to life with powerful JavaScript plugins.', category: 'Frontend' },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <postContext.Provider value={{ listPost: listPosts }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={PostPage}></Route>
+        </Routes>
+      </BrowserRouter>
+    </postContext.Provider>
   )
 }
 
---
+++ react-context-api/src/components/PostPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PostsList from './PostList'
+
+function PostPage() {
+  return (
+    <PostsList/>
+  )
+}
+
+export default PostPage
